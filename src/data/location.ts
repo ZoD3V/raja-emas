7728d23932f0226eabe45838fcd1ef5ddc3be1d0
adhi <adzkiyadzikrullah@gmail.com>
2025-05-29 12:04:18 +0700

    fix location

--- src/data/location.ts
+++ src/data/location.ts
@@ -8,7 +8,7 @@ export const locationData = {
       area: "Area Jakarta",
       data: [
         {
-          title: "Outlet Batavia PIK",
+          title: "Jakarta Utara - Batavia PIK",
           address:
             "Rukan Golf Island Blok C No.36, Pantai Indah Kapuk, Jakarta Utara.",
           phone: "0822-9999-5125",
@@ -19,34 +19,67 @@ export const locationData = {
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31738.101216564995!2d106.738746!3d-6.095281000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1d3d64d22615%3A0xef9f6e0126098642!2sRaja%20Emas%20Indonesia%20PIK%20(%20Batavia%20-%20Pantai%20Indah%20Kapuk%20)!5e0!3m2!1sen!2sus!4v1746959037782!5m2!1sen!2sus",
         },
         {
-          title: "Outlet Cikini",
+          title: "Jakarta Utara - Kelapa Gading",
           address:
-            "Cikini Gold Center Lt. UG.17, Jl. Pegangsaan Timur No.1, RT.1/RW.1, Menteng, Jakarta 10310",
-          phone: "021-21232275",
-          whatsapp: "0821-9999-9696",
-          hours: "10.00 - 19.00 WIB",
-          linkWhatsapp: "https://wa.me/6282199999696",
+            "LC6 / 38, Jl. Boulevard Bar. Raya No.53, Klp. Gading Bar. Jkt Utara, Daerah Khusus Ibukota Jakarta 14240.",
+          phone: "0822-9999-5041",
+          whatsapp: "0822-9999-5041",
+          hours: "Pukul 10.00 - 21.00 WIB",
+          linkWhatsapp: "https://wa.me/6282299995125",
           mapEmbed:
-            "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31731.97558746967!2d106.841866!3d-6.197976000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f50cf2e192f5%3A0xbdcb801ada33ca93!2sRaja%20Emas%20Indonesia%20Cikini%20Gold%20Center!5e0!3m2!1sen!2sus!4v1745725925699!5m2!1sen!2su",
+            "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31734.357763081498!2d106.902395!3d-6.158242!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f54e11b12767%3A0xba229ae3138099b5!2sRaja%20Emas%20Indonesia%20-%20Kelapa%20Gading!5e0!3m2!1sen!2sus!4v1748494678325!5m2!1sen!2sus",
         },
         {
-          title: "Outlet Melawai - Blok M",
+          title: "Jakarta Selatan - Pondok Indah",
+          address:
+            "Jl. Metro Pondok Indah No.161 Blok Tb27, Pd. Pinang, Kec. Kby. Lama, akarta Selatan, 12310.",
+          phone: "0822-9999-5034",
+          whatsapp: "0822-9999-5034",
+          hours: "Pukul 09.00 - 20.00 WIB",
+          linkWhatsapp: "https://wa.me/6282299995034",
+          mapEmbed:
+            "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31727.58516108334!2d106.783143!3d-6.27055!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1124c2c5d7d%3A0xf353f636adcef6f2!2sRaja%20Emas%20Indonesia%20Pondok%20Indah!5e0!3m2!1sen!2sus!4v1748494786104!5m2!1sen!2sus",
+        },
+        {
+          title: "Jakarta Selatan - Plaza Melawai Blok M",
           address:
             "Melawai Plaza, Jl. Melawai Raya.13 18, Blok T No.264 Lantai 1, Melawai, Kec. Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12160",
           phone: "0822 9999 5127",
           whatsapp: "0822 9999 5127",
-          hours: "10.00 - 19.00 WIB",
+          hours: "Pukul 10.00 - 19.00 WIB",
           linkWhatsapp: "https://wa.me/6282299995127",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31729.1148806567!2d106.802193!3d-6.245359!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f101f624e837%3A0x9b7f3d7e6f130b5!2sRaja%20Emas%20Indonesia%20Melawai%20-%20Blok%20M!5e0!3m2!1sen!2sus!4v1745726366875!5m2!1sen!2sus",
         },
         {
-          title: "Outlet Cengkareng",
+          title: "Jakarta Pusat - Petojo",
+          address:
+            "Jl. Petojo Melintang No.26A, Petojo Sel., Kecamatan Gambir, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10160",
+          phone: "0822-9999-5035",
+          whatsapp: "0822-9999-5035",
+          hours: "Pukul 09.00 - 20.00 WIB",
+          linkWhatsapp: "https://wa.me/6282299995035",
+          mapEmbed:
+            "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31731.97558746967!2d106.841866!3d-6.197976000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f50cf2e192f5%3A0xbdcb801ada33ca93!2sRaja%20Emas%20Indonesia%20Cikini%20Gold%20Center!5e0!3m2!1sen!2sus!4v1745725925699!5m2!1sen!2su",
+        },
+        {
+          title: "Jakarta Pusat - Cikini",
+          address:
+            "Cikini Gold Center Lt. UG.17, Jl. Pegangsaan Timur No.1, RT.1/RW.1, Menteng, Jakarta 10310",
+          phone: "021-21232275",
+          whatsapp: "0821-9999-9696",
+          hours: "Pukul 10.00 - 19.00 WIB",
+          linkWhatsapp: "https://wa.me/6282199999696",
+          mapEmbed:
+            "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31731.97558746967!2d106.841866!3d-6.197976000000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f50cf2e192f5%3A0xbdcb801ada33ca93!2sRaja%20Emas%20Indonesia%20Cikini%20Gold%20Center!5e0!3m2!1sen!2sus!4v1745725925699!5m2!1sen!2su",
+        },
+        {
+          title: "Jakarta Barat - Cengkareng",
           address:
             "Ruko Sedayu Square, Jalan Kamal Raya Outer Ring Road No.H08, Cengkareng Bar., Kecamatan Cengkareng, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11730",
           phone: "021-52392506",
           whatsapp: "0822 9999 7763",
-          hours: "09.00 - 20.00 WIB",
+          hours: "Pukul 09.00 - 20.00 WIB",
           linkWhatsapp: "https://wa.me/6282299997763",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d87005.48460536895!2d106.72243607373183!3d-6.135862967046097!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1dde8300d0f1%3A0x76655c56df5894b4!2sRaja%20Emas%20Indonesia%20Cengkareng!5e0!3m2!1sen!2sus!4v1745726469522!5m2!1sen!2sus",
@@ -62,7 +95,7 @@ export const locationData = {
             "ITC Depok, Samping Terminal Terpadu Gedoran Depok, LT.dasar Blok C5, Jl. Margonda No.56, Depok, Kec. Pancoran Mas, Kota Depok, Jawa Barat 16431",
           phone: "021-21232275",
           whatsapp: "0821-9999-9696",
-          hours: "10.00 - 20.00 WIB",
+          hours: "Pukul 10.00 - 20.00 WIB",
           linkWhatsapp: "https://wa.me/6282299997767",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31720.12686222181!2d106.823315!3d-6.391955!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69eb89f8faa041%3A0xd46f3498dbcaef92!2sRaja%20Emas%20Indonesia%20Depok!5e0!3m2!1sen!2sus!4v1745726876383!5m2!1sen!2sus",
@@ -78,7 +111,7 @@ export const locationData = {
             "Pasar Kebon Kembang, Blok F Trade Center Pasar Kebon Kembang Lantai Dasar No.194, Paledang, Bogor Tengah, Kota Bogor, Jawa Barat 16122",
           phone: "0822-9999-3067",
           whatsapp: "0822-9999-3067",
-          hours: "09.00 - 17.00 WIB",
+          hours: "Pukul 09.00 - 17.00 WIB",
           linkWhatsapp: "https://wa.me/6282299993067",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31707.48459018412!2d106.792264!3d-6.59266!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c50e93b76f37%3A0x6431bc1e47856e14!2sRaja%20Emas%20Indonesia%20Bogor!5e0!3m2!1sen!2sus!4v1745726527175!5m2!1sen!2sus",
@@ -89,23 +122,23 @@ export const locationData = {
       area: "Area Bekasi",
       data: [
         {
-          title: "Outlet Mega Bekasi Hypermall",
+          title: "Bekasi - Mega Bekasi Hypermall",
           address:
             "Mega Bekasi Hypermall,Lt.UG.133, Jl. Ahmad Yani No.1, RT.004/RW.001, Marga Jaya, Kota Bks, Jawa Barat 17141",
           phone: "0822-9999-3167",
           whatsapp: "0822-9999-3167",
-          hours: "10.00 - 20.00 WIB",
+          hours: "Pukul 10.00 - 20.00 WIB",
           linkWhatsapp: "https://wa.me/6282299993167",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d63457.72331431257!2d106.992621!3d-6.249536!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698d0fa51bfd25%3A0x9e7ca4c3fcdae4dd!2sRaja%20Emas%20Indonesia%20Mega%20Bekasi%20Hypermall!5e0!3m2!1sen!2sus!4v1745727998921!5m2!1sen!2sus",
         },
         {
-          title: "Outlet Cibubur",
+          title: "Bekasi - Cibubur",
           address:
             "Plasa Cibubur Lt.1 No.19.,Jl. Transyogi, RT.001/RW.003, Jatikarya, Kec. Jatisampurna, Kota Bks, Jawa Barat 17435",
           phone: "0822-9999-5026",
           whatsapp: "0822-9999-5026",
-          hours: "10.00 - 21.30 WIB",
+          hours: "Pukul 10.00 - 21.30 WIB",
           linkWhatsapp: "https://wa.me/6282299995026",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31721.105507018605!2d106.915243!3d-6.376156!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e699307d2937207%3A0xc88ab420b13df594!2sRaja%20Emas%20Indonesia%20-%20Plasa%20Cibubur%20(%20Opening%205%20Maret%202025%20)!5e0!3m2!1sen!2sus!4v1746960125807!5m2!1sen!2sus",
@@ -116,34 +149,34 @@ export const locationData = {
       area: "Area Tanggerang",
       data: [
         {
-          title: "Outlet Tangerang",
+          title: "Tangerang - Aeropolis",
           address:
             "ruko apartemen, Aeropolis Commercial Park 2 A2. GT.06, Jl. Aeropolis, RT.004/RW.008, Neglasari, Kec. Neglasari, Kota Tangerang, Banten 15129",
           phone: "0822-9999-7765",
           whatsapp: "0822-9999-7765",
-          hours: "10.00 - 18.00 WIB",
+          hours: "Pukul 10.00 - 18.00 WIB",
           linkWhatsapp: "https://wa.me/6282299997765",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d63470.39385688982!2d106.630975!3d-6.144184!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f962d8f01ba9%3A0x3f261d5896645e64!2sRaja%20Emas%20Indonesia%20Aeropolis%20-%20Tangerang!5e0!3m2!1sen!2sus!4v1745726936445!5m2!1sen!2sus",
         },
         {
-          title: "Outlet Mall ITC BSD",
+          title: "Tangerang Selatan - Mall ITC BSD",
           address:
             "Mall ITC BSD Tangsel, Jl. Pahlawan Seribu No.12 LT. Dasar Blok C3 No.3A-5, Lengkong Wetan, Kec. Serpong, tangerang, Banten 15310",
           phone: "0822-9999-3248",
           whatsapp: "0822-9999-3248",
-          hours: "10.00 - 20.00 WIB",
+          hours: "Pukul 10.00 - 20.00 WIB",
           linkWhatsapp: "https://wa.me/6282299993248",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31726.637889293303!2d106.664132!3d-6.286099!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fbba703e37ef%3A0x1781c609e507746d!2sRaja%20Emas%20Indonesia%20Mall%20ITC%20BSD!5e0!3m2!1sen!2sus!4v1745727092262!5m2!1sen!2sus",
         },
         {
-          title: "Outlet Tangcity Mall",
+          title: "Tangerang - Tangcity Mall",
           address:
             "Tangcity Mall, Jl. Jenderal Sudirman.1 Lantai LG D-09, Cikokol, Kec. Tangerang, Banten 15117",
           phone: "0822-9999-4127",
           whatsapp: "0822-9999-4127",
-          hours: "10.00 - 21.30 WIB",
+          hours: "Pukul 10.00 - 21.30 WIB",
           linkWhatsapp: "http://wa.me/6282299994127",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31732.21825444565!2d106.63405!3d-6.19394!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f9daabedeb19%3A0xc5c9c4d0e1605421!2sRaja%20Emas%20Indonesia%20Tangcity%20Mall%20Tangerang!5e0!3m2!1sen!2sus!4v1745728066603!5m2!1sen!2sus",
@@ -154,18 +187,18 @@ export const locationData = {
       area: "Area Bandung",
       data: [
         {
-          title: "Outlet Bandung",
+          title: "Bandung-Sukajadi",
           address:
             "Jl. Sukajadi No.158, Cipedes, Kel. Pasteur, Kec. Sukajadi, Kota Bandung, Jawa Barat 40162",
           phone: "0822-9999-7764",
           whatsapp: "0822-9999-7764",
-          hours: "10.00 - 20.00 WIB",
+          hours: "Pukul 10.00 - 20.00 WIB",
           linkWhatsapp: "https://wa.me/6282299997764",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d63376.45635587361!2d107.596938!3d-6.887187!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e7a7c15acbd1%3A0x94cf82e2d4aa77a2!2sRaja%20Emas%20Indonesia%20Bandung!5e0!3m2!1sen!2sus!4v1745727729023!5m2!1sen!2sus",
         },
         {
-          title: "Outlet Kopo Bandung",
+          title: "Bandung-Kopo",
           address:
             "Jl. Raya Kopo No.632, Babakan Ciparay, Kec. Babakan Ciparay, Kota Bandung, Jawa Barat 40225",
           phone: "0822-9999-5042",
@@ -181,12 +214,12 @@ export const locationData = {
       area: "Surabaya",
       data: [
         {
-          title: "Outlet Pakuwon Trade Center Surabaya",
+          title: "Surabaya - Pakuwon Trade Center",
           address:
             "Pakuwon Trade Center Mall Surabaya, Jl. Raya Lontar No.2 D2- 06 lt GF, Babatan, Kec. Wiyung, Surabaya, Jawa Timur 60123",
           phone: "0822-9999-4123",
           whatsapp: "0822-9999-4123",
-          hours: "10.00 - 21.30 WIB",
+          hours: "Pukul 10.00 - 21.30 WIB",
           linkWhatsapp: "https://wa.me/6282299994123",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31660.48535578056!2d112.674247!3d-7.290721!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fdfb49e8d22b%3A0xb7bc943f67a95de7!2sRaja%20Emas%20Indonesia%20Pakuwon%20Trade%20Center%20Mall%20Surabaya!5e0!3m2!1sen!2sus!4v1745728116110!5m2!1sen!2sus",
@@ -202,7 +235,7 @@ export const locationData = {
             "Medan Mall, Jl. M. T. Haryono No.21 Lt Dasar, Pusat Ps., Kec. Medan Kota, Kota Medan, Sumatera Utara 20212",
           phone: " 0822-9999-3238",
           whatsapp: " 0822-9999-3238",
-          hours: "09.00 - 18.00 WIB",
+          hours: "Pukul 09.00 - 18.00 WIB",
           linkWhatsapp: "https://wa.me/6282299993238",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31855.97991528154!2d98.687088!3d3.5880500000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031310195297d1d%3A0x7c791a3e336eedf7!2sRaja%20Emas%20Indonesia%20Medan%20Mall!5e0!3m2!1sen!2sus!4v1745727654150!5m2!1sen!2sus",
@@ -218,7 +251,7 @@ export const locationData = {
             "Jl. Hasanuddin No.54, Dauh Puri Kangin, Kec. Denpasar Bar., Kota Denpasar, Bali 80232",
           phone: "0822-9999-3136",
           whatsapp: "0822-9999-3136",
-          hours: "10.00 - 20.00 WITA",
+          hours: "Pukul 10.00 - 20.00 WITA",
           linkWhatsapp: "https://wa.me/6282299993136",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31554.77385235835!2d115.213922!3d-8.658536!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2413e8862f02f%3A0xc8efda411499288e!2sRaja%20Emas%20Indonesia%20Bali!5e0!3m2!1sen!2sus!4v1745727795687!5m2!1sen!2sus",
@@ -234,7 +267,7 @@ export const locationData = {
             "Jl. Pejanggik No.8, Cakranegara Bar., Kec. Cakranegara, Mataram, Lombok, Nusa Tenggara Bar. 83231",
           phone: "0822-9999-3018",
           whatsapp: "0822-9999-3018",
-          hours: "10.00 - 18.00 WITA",
+          hours: "Pukul 10.00 - 18.00 WITA",
           linkWhatsapp: "https://wa.me/6282299993018",
           mapEmbed:
             "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31560.740239209066!2d116.12922900000001!3d-8.587102!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcdbf12d93a9423%3A0xe6b1acca3c8fd923!2sRaja%20Emas%20Indonesia%20Lombok!5e0!3m2!1sen!2sus!4v1745727945185!5m2!1sen!2sus",
